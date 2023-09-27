ea58527a2e517d0fe2cef2720fcd144539462d16
Rathnasingam Kesavaram <rathnasingam.kesav@hcl.com>
2023-09-27 10:57:16 +0530

    Add project files.

---
+++ AuthProject/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option =>
+    {
+        option.LoginPath = "/Access/Login";
+        option.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Access}/{action=Login}/{id?}");
+
+app.Run();
